8619b082aa2decd39fe1b3390d53075d9f354b57
Alex Akama <gremcox@bk.ru>
2020-08-05 10:45:52 +0500

    STAGE 1

---
+++ src/main/java/main/DefaultController.java
@@ -0,0 +1,15 @@
+package main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+}
