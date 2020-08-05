b1f5e31b73279e9a20376cde120f24b5bac8603d
Alex Akama <gremcox@bk.ru>
2020-08-05 20:16:08 +0500

    Hotfix 1

--- src/main/java/controller/DefaultController.java
+++ src/main/java/controller/DefaultController.java
@@ -1,9 +1,8 @@
-package main;
+package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DefaultController {
