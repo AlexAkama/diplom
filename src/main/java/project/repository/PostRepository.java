12100c7c097423faaf08e0555ff66648f4ac853e
Alex Akama <gremcox@bk.ru>
2021-04-11 16:51:00 +0500

    Updated HQL

--- src/main/java/project/repository/PostRepository.java
+++ src/main/java/project/repository/PostRepository.java
@@ -3,8 +3,8 @@ package project.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import project.dto.global.MapDto;
 import project.dto._post.PostYearDto;
+import project.dto.global.MapDto;
 import project.model.Post;
 
 import java.util.List;
@@ -12,12 +12,16 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query("SELECT DATE_FORMAT(p.time, '%Y') AS year FROM Post p GROUP BY year ORDER BY year DESC")
+    String year = "function('DATE_FORMAT', p.time, '%Y')";
+    String date = "function('DATE_FORMAT', p.time, '%Y-%m-%d')";
+
+    @Query("SELECT " + year + " AS year FROM Post p GROUP BY year ORDER BY " + year + " DESC")
     List<PostYearDto> getYearList();
 
-    @Query("SELECT DATE_FORMAT(p.time, '%Y-%m-%d') AS key, COUNT(p) AS value FROM Post p"
-            + " WHERE YEAR(p.time) = ?1 AND is_active=1 AND moderation_status='ACCEPTED' AND time<NOW()"
-            + " GROUP BY key ORDER BY key DESC")
+    @Query("SELECT " + date + " AS key, COUNT(p) AS value FROM Post p"
+            + " WHERE function('YEAR', p.time) = ?1 AND p.isActive=true AND p.moderationStatus='ACCEPTED'"
+            + " AND p.time < function('NOW')"
+            + " GROUP BY key ORDER BY " + date + " DESC")
     List<MapDto> getPostCounterList(int year);
 
 }
