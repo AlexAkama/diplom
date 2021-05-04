94a1b3806cf98210a6389b44e3165e25dade4c06
Alex Akama <gremcox@bk.ru>
2021-05-04 08:31:04 +0500

    Added Exception Handler

--- src/main/java/project/exception/AppException.java
+++ src/main/java/project/exception/AppException.java
@@ -2,7 +2,7 @@ package project.exception;
 
 abstract class AppException extends Exception {
 
-    protected final String description;
+    private final String description;
 
     public AppException(String description) {
         this.description = description;
--- src/main/java/project/exception/GlobalExceptionHandler.java
+++ src/main/java/project/exception/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package project.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import project.dto.main.Message;
+
+import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -10,5 +14,39 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ImageSuccess.class)
     public ResponseEntity<String> handleImage(ImageSuccess e) {
         return ResponseEntity.ok(e.getPath());
+    }
+
+    //400
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Message> handleContentBadRequest(BadRequestException e) {
+        return getHandlerResponse(e, BAD_REQUEST);
+    }
+
+    //401
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<Message> handleContentUnauthorized(UnauthorizedException e) {
+        return getHandlerResponse(e, UNAUTHORIZED);
+    }
+
+    //403
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<Message> handleContentForbidden(ForbiddenException e) {
+        return getHandlerResponse(e, FORBIDDEN);
+    }
+
+    //404
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Message> handleContentNotFound(NotFoundException e) {
+        return getHandlerResponse(e, NOT_FOUND);
+    }
+
+    //500
+    @ExceptionHandler(InternalServerException.class)
+    public ResponseEntity<Message> handleContentInternalServerError(InternalServerException e) {
+        return getHandlerResponse(e, INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<Message> getHandlerResponse(AppException e, HttpStatus status) {
+        return new ResponseEntity<>(new Message(e.getDescription()), status);
     }
 }
